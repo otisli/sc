c07c093b282e1244dc18a5d5d6e8292ba85e5c72
李政昌 <otislee@gamasys.com.tw>
2018-12-19 15:55:38 +0800

    Initial Commit

---
+++ sc/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  sc
+//
+//  Created by 李政昌 on 2018/12/19.
+//  Copyright © 2018年 otislee. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
